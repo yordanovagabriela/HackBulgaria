0ccf7c2b6856dc2ea80b3b5b1571c07431f5315b
Gabriela <gabriela.georgieva.yordanova@gmail.com>
2015-04-02 00:36:53 +0300

    new tests

--- week3/bank_account.py
+++ week3/bank_account.py
@@ -4,28 +4,42 @@ class BankAccount:
         self.name = name
         self.balance = balance
         self.currency = currency
+        self.account_history = ["Account was created"]
+
+    def history(self):
+        return self.account_history
 
     def deposit(self, amount):
         if amount < 0:
             raise ValueError
         else:
             self.balance += amount
+            deposit_history = 'Deposited ' + str(amount) + self.currency
+            self.account_history.append(deposit_history)
 
     def itsbalance(self):
+        balance_history = 'Balance check -> ' + str(self.balance) + self.currency
+        self.account_history.append(balance_history)
         return self.balance
 
     def withdraw(self, amount):
         if amount <= self.balance:
-            return True
             self.balance -= amount
+            withdraw_history = str(amount) + self.currency + ' was withdrawed'
+            self.account_history.append(withdraw_history)
+            return True
         else:
+            withdraw_history = 'Withdraw for {}{} failed.'.format(
+                amount, self.currency)
+            self.account_history.append(withdraw_history)
             return False
-        self.balance -= self.amount
 
     def __str__(self):
         return "Bank account for {} with balance of {}{}".format(self.name, self.balance, self.currency)
 
     def __int__(self):
+        int_history = '__int__ check -> ' + str(self.balance) + self.currency
+        self.account_history.append(int_history)
         return self.balance
 
     def transfer_to(self, account, amount):
@@ -33,6 +47,12 @@ class BankAccount:
             raise ValueError
         if self.balance < amount:
             return False
+        account1 = 'Transfer to {} for {}{}'.format(
+            account.name, amount, self.currency)
+        account2 = 'Transer from {} for {}{}'.format(
+            self.name, amount, account.currency)
+        self.account_history.append(account1)
+        account.account_history.append(account2)
         self.balance -= amount
         account.balance += amount
         return True
@@ -46,7 +66,4 @@ print(int(account))
 print(account.withdraw(2000))
 rado = BankAccount("Rado", 600, "BGN")
 ivo = BankAccount("Ivo", 0, "BGN")
-print(rado.transfer_to(ivo, 700))
-print(rado.balance)
-print(ivo.balance)
-
+print(account.history())
--- week3/bank_account_test.py
+++ week3/bank_account_test.py
@@ -1,41 +1,44 @@
 from bank_account import BankAccount
+
+
 import unittest
 
 
-class TestBankAccount(unnittest.TestCase):
+class TestBankAccount(unittest.TestCase):
+
+    def setUp(self):
+        self.my_account = BankAccount("Ivo", 100, "$")
 
     def test_init(self):
-        my_account = BankAccount("Ivo", 100, "$")
-        self.assertequal(my_account.name, "ime")
+        self.assertEqual(self.my_account.name, "Ivo")
 
     def test_str(self):
-        needed_result = BankAccount("Ivo", 100, "$")
-        self.assertequal(str(my_account), needed_result)
+        needed_result = "Bank account for Ivo with balance of 100$"
+        self.assertEqual(str(self.my_account), needed_result)
 
     def test_deposite(self):
         self.my_account.deposit(100)
-        self.assertequal(self.my_account.balance, 200)
+        self.assertEqual(self.my_account.balance, 200)
         with self.assertRaises(ValueError):
             self.my_account.deposit(-20)
+
+    def test_balance(self):
+        self.assertEqual(self.my_account.balance, 100)
 
     def test_transfer_to(self):
         your_account = BankAccount("rado", 10, "&")
         with self.assertRaises(ValueError):
-            self.my_account.trasfer_to(your_account, 200)
-        self.assertequal(self.my_account.balance, 10000)
-        self.assertequal(self.your_account, 10)
+            self.my_account.transfer_to(your_account, 200)
+        self.assertEqual(self.my_account.balance, 100)
+        self.assertEqual(your_account.balance, 10)
 
     def transfer_more_money_than_we_have(self):
         your_account = BankAccount("rado", 10, "&")
         self.assertFalse(your_account.trasfer_to(self.my_account, 300))
-        self.assertequal(self.my_account.balance, 1000000)
-        self.assertequal(your_account.balance, 10)
+        self.assertEqual(self.my_account.balance, 1000000)
+        self.assertEqual(your_account.balance, 10)
 
-    def test_transfer_to(self):
-        your_account = BankAccount("rado", 10, "$")
-        result = self.my_account.trasfer_to(your_account, 300)
-        self.assertequal(your_account.balance, 310)
-        self.assertequal(self.my_account.balance, 1000000 - 300)
-        self.assertrue(result)
-if __name__ == 'main':
+    def test_withdraw(self):
+
+if __name__ == '__main__':
     unittest.main()
