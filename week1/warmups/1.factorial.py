d7a5d1bad322ae632b78eed5ec6840c1a454f27c
Gabriela <gabriela.georgieva.yordanova@gmail.com>
2015-04-10 01:41:49 +0300

    factorial

--- week1/warmups/1.factorial.py
+++ week1/warmups/1.factorial.py
@@ -1,6 +1,10 @@
 def factorial(n):
+    if n == 0:
+        return 1
+    if n < 0:
+        raise ValueError
     result = 1
     for i in range(1, n+1):
         result *= i
-        return result
-print(factorial(5))
+    return result
+print(factorial(-10))
